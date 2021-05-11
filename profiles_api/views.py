3c43a2bab2dca1760a0fd7ed332ccd83352f504e
Yash Gaikwad <yashgaikwad12112000@gmail.com>
2021-05-11 19:13:10 +0530

    Added POST, PUT, PATCH & DELTE Methods to our Hello API View

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,5 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response # when calling the APIVIew it will return you the standard object
+from rest_framework import status # returns HTTP status code with the APIs called
+
+from profiles_api import serializers
 
 
 #APIView class
@@ -8,6 +11,8 @@ from rest_framework.response import Response # when calling the APIVIew it will 
 # from HTTP requests
 class HelloAPIView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
+
 
     #aading an HTTP funtion to an APIVIew
     def get(self, response, format=None):
@@ -20,3 +25,34 @@ class HelloAPIView(APIView):
         ]
 
         return Response({'message':"Hello!", 'an_apiview':an_apiview})
+
+    # Adding post functionality to our API view
+    def post(self, request):
+        """Creating a hello message with our name"""
+
+        #storing in a varibale
+        serializer = self.serializer_class(data= request.data) #built-in function which requests the data
+
+        #validating the datab
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors,
+            status = status.HTTP_400_BAD_REQUEST
+            )
+
+    # it updates the entire object
+    def put(self, request, pk=None): # pk - primary key - to take the ID of the object
+        """Handle updating an object"""
+        return Response({"Method":"PUT"})
+
+    # handle the partail update
+    def patch(self, request, pk=None):
+        """handle the parital update of the object, used for updating fields of the object"""
+        return Response({'method':"PATCH"})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method':'DELETE'})
